7794cd62680c643432e0d9749a570b4e745fbd1c
jonathan1803 <jonathan.landaverde18@gmail.com>
2024-09-06 21:56:42 -0600

    Add: layout-nested

---
+++ src/01-lazyload/pages/NoLazy.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NoLazyLoad = () => {
+  return (
+    <div>
+        <h1>NoLazyLoad</h1>
+    </div>
+  )
+}
+
+export default NoLazyLoad
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,4 +1,5 @@
 import { lazy, LazyExoticComponent } from 'react'
+import NoLazyLoad from '../01-lazyload/pages/NoLazy';
 type JSXComponent = () => JSX.Element
 
 interface Route {
@@ -8,27 +9,20 @@ interface Route {
   name: string;
 }
 
-const Lazy1= lazy(/*"LazyPage1" */ () => import('../01-lazyload/pages/LazyPage1'))
-const Lazy2= lazy(/*"LazyPage2" */ () => import('../01-lazyload/pages/LazyPage2'))
-const Lazy3= lazy(/*"LazyPage3" */ () => import('../01-lazyload/pages/LazyPage3'))
+const LazyLayout= lazy(/*"LazyPage1" */ () => import('../01-lazyload/layout/LazyLayout'))
 
 export const routes: Route[] = [
   {
-    to: '/lazy1',
-    path: 'lazy1',
-    Component: Lazy1,
-    name: 'Lazy-1',
+    path: '/lazy-layout/*',
+    to: 'lazy-layout',
+    Component: LazyLayout,
+    name: 'lazy-layout',
   },
   {
-    to: '/lazy2',
-    path: 'lazy2',
-    Component: Lazy2,
-    name: 'Lazy-2',
+    to: '/no-lazy',
+    path: 'no-lazy',
+    Component: NoLazyLoad,
+    name: 'no-lazy',
   },
-  {
-    to: '/lazy3',
-    path: 'lazy3',
-    Component: Lazy3,
-    name: 'Lazy-3',
-  },
+  
 ]
